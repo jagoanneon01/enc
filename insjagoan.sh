877710882f02a8a480eb6bc027a28e6f54265a41
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-12-21 12:09:12 +0700

    First Commit

--- insjagoan.sh
+++ insjagoan.sh
@@ -8,17 +8,17 @@ cyan="\e[1;36m"
 NC="\e[1;37m"
 clear
 figlet -f small ENCRYPT Bash By Ronggo86 X MH SSH SEDANG | lolcat
-sleep 2
 echo ""
-echo -e "${biru}Bismillah Dulu Biar Berkah"
-read -p " Masukkan Nama File : " file
-enc=$(shc -r -f $file && rm $file && rm $file.x.c && mv $file.x $file && chmod +x $file )
+echo -e "RONGGOLAWE REBORN ENCRYPT BASH" | lolcat
+echo -e "FB : Generasi Ronggolawe Tuban" | lolcat
+read -rp">>>>Masukkan Nama File : " file
+enc=$(shc -r -f $file && rm $file && rm $file.x.c && mv $file.x $file && chmod >
 read -p " Klik Enter Untuk Melanjutkan : " $enc
 echo ""
 echo -e " ${cyan} Tunggu Sebentar Sedang Proses >>>>> "
 sleep 3
 echo ""
-echo -e " ${kuning} Encrypt Sukses Bro :) "
+echo -e " ${kuning}xxxxEncrypt Sukses Bro :)xxxx"
 echo ""
 exit 0
 fi
